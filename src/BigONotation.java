4c9938f4a1f501969594f57f1f6ba57ec162a8b2
knightch <amankumarpandey2397@gmail.com>
2022-06-30 23:29:09 +0530

    log(N) complexity code example

--- src/BigONotation.java
+++ src/BigONotation.java
@@ -5,7 +5,16 @@ public class BigONotation {
             i = i * 2;
         }
     }
+
+    public static void halvingLoopVariable(int n){
+        for(int i = n; i>0;){
+            System.out.println("Value of i is:"+i);
+            i = i / 2;
+        }
+    }
+
     public static  void main(String[] args){
-        doublingLoopVariable(100000000);
+//        doublingLoopVariable(100);
+        halvingLoopVariable(100);
     }
 }
